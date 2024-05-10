63b68e18811b813b3758ac6be376013ce59211d8
Mateo Saavedra <mateo.saavedrar.est@uets.edu.ec>
2024-05-10 11:10:33 -0500

    Primeros cambios, camara, guardar en galeria

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo} from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
 
   constructor() {}
 
+  ngOnInit(): void {
+    Camera.requestPermissions();  
+  }
+
+  imagenParaMostrar:string="";
+  photo: Photo | undefined;
+  imagenTomada:any; 
+  
+  async getPicture() {
+    this.imagenTomada = await Camera.getPhoto({
+      quality: 90,
+      resultType: CameraResultType.Uri,
+      source:CameraSource.Camera,
+      saveToGallery: true
+    });
+    
+    if(this.imagenTomada){
+			this.imagenParaMostrar = this.imagenTomada.webPath
+    }
+  }
+
+  async openGallery() {
+    try {
+      const galleryPhoto = await Camera.getPhoto({
+        quality: 90,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Photos
+      });
+
+      if (galleryPhoto && galleryPhoto.webPath) {
+        this.photo = galleryPhoto;
+        this.imagenParaMostrar = galleryPhoto.webPath;
+      } else {
+        console.error("No se seleccionó ninguna imagen de la galería.");
+      }
+    } catch (e) {
+      console.error("Error al abrir la galería de imágenes:", e);
+    }
+  }
 }
